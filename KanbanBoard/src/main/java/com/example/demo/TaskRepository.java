fe4d07286a5b02cd5c1bf3631ba24746c9c6b4dd
cscx1 <154300319+cscx1@users.noreply.github.com>
2024-01-28 14:13:05 -0500

    Delete KanbanBoard directory

--- KanbanBoard/src/main/java/com/example/demo/TaskRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TaskRepository extends MongoRepository<Task, String> {
-    // Additional methods if needed
-}
